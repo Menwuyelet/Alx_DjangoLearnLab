eab8a6d3dfa2eaea7b0e012ed3861f7294679383
Menwuyelet <menutemesgen@gmail.com>
2024-08-17 14:37:54 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -54,7 +54,7 @@ def is_admin(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, "relationship_app/admin_view.html")
 
 
 def is_librarian(user):
@@ -62,7 +62,7 @@ def is_librarian(user):
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, "relationship_app/librarian_view.html")
 
 
 
@@ -71,4 +71,7 @@ def is_member(user):
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, "relationship_app/member_view.html")
+
+
+
