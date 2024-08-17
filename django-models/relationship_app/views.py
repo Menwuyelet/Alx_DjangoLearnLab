1febf99f98ea9277f85700504e7cacf92e58df2f
Menu Temesgen <menutemesgen@gmail.com>
2024-08-17 12:40:22 +0300

    added the views.py.

---
+++ django-models/relationship_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
+# Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    context = {'book_list': books}
+    return render(request, 'books/book_list.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'libraries/library_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
