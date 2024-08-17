6153e0e250783014218b53ea5117897f72089e95
Menwuyelet <menutemesgen@gmail.com>
2024-08-17 14:05:12 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,7 +4,7 @@ from .views import LibraryDetailView
 from views import register
 from .views import LogoutView
 from .views import LoginView
-from .views import Admin
+from .views import admin_view
 from .views import librarian_view
 from .views import member_view
 
@@ -15,7 +15,7 @@ urlpatterns = [
     path('register/', views.register.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/login.html'), name='logout'),
-    path('Admin/', Admin, name='Admin'),
+    path('admin/', admin_view, name='Admin'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
 ]
