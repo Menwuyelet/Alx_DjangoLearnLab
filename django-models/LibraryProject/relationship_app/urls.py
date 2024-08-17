980108fb49a9f7f5c709427f7bf56a8514b3b061
Menu Temesgen <menutemesgen@gmail.com>
2024-08-17 12:28:30 +0300

    updated

--- django-models/LibraryProject/relationship_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('book_list/', views.book_list, name='book_list'),
-    path('LibraryDetailView/', views.AboutView.as_view(), name='about'),
-]
--- django-models/relationship_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import list_books
-from .views import LibraryDetailView
-
-urlpatterns = [
-    path('book_list/', list_books, name='book_list'),
-    path('LibraryDetailView/', LibraryDetailView.as_view(), name='about'),
-]
