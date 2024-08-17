9021317f0cc1f2694ee3b8e3a86992b284a4c759
Menwuyelet <menutemesgen@gmail.com>
2024-08-17 13:23:26 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
-from .views import RegistrationView
+from views import register
 from .views import LogoutView
 from .views import LoginView
 urlpatterns = [
     path('list_books/', list_books, name='list_books'),
     path('LibraryDetailView/', LibraryDetailView.as_view(), name='LibraryDetailView'),
-    path('register/', RegistrationView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register.as_view(), name='register'),
+    path('login/', LoginView.as_view(tamplate_name = login), name='login'),
+    path('logout/', LogoutView.as_view(tamplate_name = logout), name='logout'),
 ]
+
+
