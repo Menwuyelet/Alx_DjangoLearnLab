5470bb178d2f229ff76b98eb6b79e271bef9a166
Menwuyelet <menutemesgen@gmail.com>
2024-08-17 14:04:37 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -53,8 +53,8 @@ def is_admin(user):
     return user.userprofile.role == 'Admin'
 
 @user_passes_test(is_admin)
-def Admin(request):
-    return render(request, 'Admin.html')
+def admin_view(request):
+    return render(request, 'admin_view.html')
 
 
 def is_librarian(user):
