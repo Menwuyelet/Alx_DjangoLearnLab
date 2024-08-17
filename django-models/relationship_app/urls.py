44e923dfd15515b681ef4a170b5063ea61242792
Menu Temesgen <menutemesgen@gmail.com>
2024-08-17 14:54:19 +0300

    created the specific permissions.

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,7 +7,9 @@ from .views import LoginView
 from .views import admin_view
 from .views import librarian_view
 from .views import member_view
-
+from .views import add_book_view
+from .views import edit_book_view
+from .views import delete_book_view
 
 urlpatterns = [
     path('list_books/', list_books, name='list_books'),
@@ -18,7 +20,12 @@ urlpatterns = [
     path('admin/', admin_view, name='Admin'),
     path('librarian/', librarian_view, name='Librarian'),
     path('member/', member_view, name='Member'),
+    path('add-book/', add_book_view, name='add_book'),
+    path('edit-book/<int:book_id>/', edit_book_view, name='edit_book'),
+    path('delete-book/<int:book_id>/', delete_book_view, name='delete_book'),
 ]
+
+
 
 
 
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -12,6 +12,10 @@ from django.views.generic.edit import CreateView
 
 from django.contrib.auth.decorators import user_passes_test
 
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+
+from .forms import BookForm
 
 # Create your views here.
 
@@ -75,3 +79,37 @@ def member_view(request):
 
 
 
+
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book_view(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list') 
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
+
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book_view(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', book_id=book.id)  
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'edit_book.html', {'form': form, 'book': book})
+
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book_view(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list') 
+    return render(request, 'delete_book.html', {'book': book})
