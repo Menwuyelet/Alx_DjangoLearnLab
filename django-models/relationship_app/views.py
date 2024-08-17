0c803b199e67ea25508842e6aa080b8c62b3b253
Menwuyelet <menutemesgen@gmail.com>
2024-08-17 13:30:34 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -30,10 +30,9 @@ class LibraryDetailView(DetailView):
 
 
 
-
 class register(CreateView):
-    form_class = UserCreationForm
-    template_name = 'templates/register.html'
+    form_class = UserCreationForm()
+    template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
 
 
