e53bc478270bce04a84aa1ccf9d8ba068d710b44
Menu Temesgen <menutemesgen@gmail.com>
2024-08-17 11:30:01 +0300

    created the views for relationship_app.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,7 +12,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return (f"{self.title} by {self.author}")
 
 class Library(models.Model):
     name = models.CharField(max_length=250)
