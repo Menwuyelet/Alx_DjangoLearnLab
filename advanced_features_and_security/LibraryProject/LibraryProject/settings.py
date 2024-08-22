01c872b0151333d04c1cc573c90fe73cf72f0ce0
Menwuyelet <menutemesgen@gmail.com>
2024-08-22 13:09:46 +0300

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'models.CustomUser'
+AUTH_USER_MODEL = "bookshelf.CustomUser"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
