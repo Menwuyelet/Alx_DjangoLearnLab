af51a347e5c3bc6482b5b92e5d292197711f78a6
Menu Temesgen <menutemesgen@gmail.com>
2024-08-22 09:31:26 +0300

    updated advanced_features_and_security.

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -7,3 +9,21 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author')
     search_fields = ('title', 'author')
 admin.site.register(Book, BookAdmin)
+
+
+
+# Register your models here.
+
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
