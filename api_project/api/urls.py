d210cb328c36b6c111ca47c273cb6b077e7c6abb
Menu Temesgen <menutemesgen@gmail.com>
2024-08-29 16:21:54 +0300

    added Booklist view and its urlpattern because task number one is giving errors.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,10 +4,15 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 router = DefaultRouter()
 router.register(r'BookList', BookViewSet)
-
+from .views import BookList
 
 
 urlpattern = [
     path('api/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api-token-auth')
+]
+
+
+urlpattern = [
+    path('books/', BookList.as_view(), name='book_list'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 # Create your views here.
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -10,3 +11,6 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
