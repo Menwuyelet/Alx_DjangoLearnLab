5fa8491bda048a7c0f88ad60edcf24f31b947139
Menu Temesgen <menutemesgen@gmail.com>
2024-09-10 10:18:43 +0300

    updated the views.py to implment authentication.

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -133,5 +132,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'accounts.User'
