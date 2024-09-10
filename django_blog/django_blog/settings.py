ea5dd9e3d4a9309138d3e5c74076b87b10cbbe78
Menu Temesgen <menutemesgen@gmail.com>
2024-09-10 10:38:38 +0300

    updated the views.py to implment authentication.

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static_files'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'blog/static_files',  
+    BASE_DIR / 'blog/static',  
 ]
 
 # Default primary key field type
